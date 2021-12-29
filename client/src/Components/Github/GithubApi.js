4c751a60899051afccdc85e0a87091eeccd93255
Avinesh Tripathi <avineshtripathi1@gmail.com>
2021-12-29 23:45:48 +0530

    printed the array on screen

--- client/src/Components/Github/GithubApi.js
+++ client/src/Components/Github/GithubApi.js
@@ -3,43 +3,30 @@ import React, {Component} from "react";
 
 class GithubApi extends Component {
   state = {
-      data: null,
-      issue: null
+      data: null
     };
   
     componentDidMount() {
-      Promise.all([this.callForExpress(), this.callForIssue()])
-        .then(([res1, res2]) => {
-          this.setState({data: res1.express, issue: JSON.stringify(res2).charAt(42)});
-        });
+
+      this.callBackendApi()
+        .then(res => this.setState({data: res})); 
     }
     
       // fetching the GET route from the Express server which matches the GET route from server.js
-    callForExpress = async () => {
-      const response = await fetch('/express');
-      const body = await response.json();
+    callBackendApi = async () => {
+      const response = await fetch('/data');
+      const body = await response.text();
   
       if (response.status !== 200) {
         throw Error(body.message) 
       }
       return body;
     };
-
-    callForIssue = async () => {
-      const response= await fetch('/issue');
-      const body = await response.json();
-
-      if(response.status !== 200) {
-        throw Error(body.message)
-      }
-      return body;
-    }
   
     render() {
       return (
         <div>
           Hi, {this.state.data}
-          {this.state.issue}
         </div>
       );
     }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 import fetch from 'node-fetch';
 import express from "express"
 
-var issue_assigned=0  
+var issue_assigned=""
+var arr = []  
 const query = `
   query {
-    viewer {
-      issues(filterBy: {assignee: "AvineshTripathi"}, last: 11) {
+    user(login: "AvineshTripathi") {
+      issues(filterBy: {assignee: "AvineshTripathi"}) {
         totalCount
+      }
+    }
+    viewer {
+      repositoriesContributedTo(first: 10, contributionTypes: COMMIT) {
+        nodes {
+          nameWithOwner
+        }
       }
     }
   }`;
@@ -15,12 +23,34 @@ fetch('https://api.github.com/graphql', {
   method: 'POST',
   body: JSON.stringify({query}),
   headers: {
-    Authorization: `Bearer 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`
+    Authorization: `Bearer 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`
   }
 })
   .then(res => res.text())
-  .then(text => issue_assigned = text)
+  .then(text => issue_assigned = text.split(','))
+  .then(issue_assigned => getArr(issue_assigned))
   .catch(error => console.error(error))
+
+
+// to convert the json array into simple array
+function getArr(issue_assigned){
+  var count = 0
+  issue_assigned.forEach(element => {
+    const f = element.substring(
+      element.indexOf(':'),
+      element.lastIndexOf('"')+2
+    )
+    if(count == 0){
+      const issueCount = element.substring(element.indexOf('t":')+3, element.lastIndexOf('}')-1)
+      arr.push(issueCount)
+      count=1
+    }else {
+      const fin = f.substring(f.indexOf(':"')+2, f.lastIndexOf('"}'))
+      arr.push(fin)
+    }    
+  });
+}
+
 
 
 const app = express();
@@ -28,11 +58,7 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`listening to port ${process.env.PORT}`))
 
-app.get('/express', (req,res) => {
-    res.send({express: 'Your express backend is connected now'});
-    console.log(req)
+app.get('/data', (req,res) => {
+    res.send(arr);
 });
 
-app.get('/issue', (req,res) => {
-  res.send(issue_assigned)
-})
